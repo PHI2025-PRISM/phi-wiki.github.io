f1596cdf57a27d2f06fa78e12d470e1e5c0a180a
PRISM_PHI <178201927+PHI2025-PRISM@users.noreply.github.com>
2025-11-29 05:34:19 +0800

    Create worker.js

---
+++ src/worker.js
@@ -0,0 +1,8 @@
+// src/worker.js
+export default {
+  async fetch(request, env, ctx) {
+    return new Response('Hello World!', {
+      headers: { 'content-type': 'text/plain' },
+    });
+  },
+};
