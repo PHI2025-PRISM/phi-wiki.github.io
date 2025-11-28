84e13fefbaa26ab94ca50f9990c3e2c81fe90152
PRISM_PHI <178201927+PHI2025-PRISM@users.noreply.github.com>
2025-11-29 05:45:09 +0800

    Delete src directory

--- src/worker.js
+++
@@ -1,8 +0,0 @@
-// src/worker.js
-export default {
-  async fetch(request, env, ctx) {
-    return new Response('Hello World!', {
-      headers: { 'content-type': 'text/plain' },
-    });
-  },
-};
